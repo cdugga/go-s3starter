31789adb92de70b9fb1970303754a55eaaf34c89
cdugga <duggan.colin@gmail.com>
2020-05-23 19:10:05 +0100

    update naming

--- cloudformation/handler/s3starter.go
+++ cloudformation/handler/s3starter.go
@@ -1,19 +1,23 @@
 package main
-
+ 
 import (
-	"encoding/json"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/sbstjn/go-lambda-example/repository"
+        "fmt"
+        "github.com/aws/aws-lambda-go/lambda"
 )
 
-
-func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	
-	return events.APIGatewayProxyResponse{Body: string("success"), StatusCode: 200}, nil
+type MyEvent struct {
+        Name string `json:"What is your name?"`
+        Age int     `json:"How old are you?"`
 }
-
+ 
+type MyResponse struct {
+        Message string `json:"Answer:"`
+}
+ 
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+        return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+}
+ 
 func main() {
-	lambda.Start(handleRequest)
+        lambda.Start(HandleLambdaEvent)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cdugga/go-s3starter
 
 go 1.14
 
-require github.com/aws/aws-sdk-go v1.30.29
+require (
+	github.com/aws/aws-lambda-go v1.17.0
+	github.com/aws/aws-sdk-go v1.30.29
+)
